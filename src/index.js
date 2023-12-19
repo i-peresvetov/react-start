9b6a05244a17d8a0d511a719f1dafb3ba99b5d3c
Ivan Peresvetov <lumenist@mail.ru>
2023-12-19 20:30:29 +0300

    dom1912

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
-import React from "react";
-// import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
+import App from './components/app'
 
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make Awesome App', important: true, id: 2},
-        {label: 'Have a lunch', important: false, id: 3},
-        {label: 'Travel to work', important: false, id: 4}
-    ]
-
-    return (
-        <div>            
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    )
-}
-// const el = React.createElement('h1', null, 'Hello World!!')
-
-// ReactDOM.render(<App/>, document.getElementById("root"));
-const root = createRoot(document.getElementById('root'))
-root.render(<App/>)
+ReactDOM.render(<App />,
+  document.getElementById('root'));
