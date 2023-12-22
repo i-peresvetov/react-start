572966db833b350107f680a9fcc906013477e843
Ivan Peresvetov <lumenist@mail.ru>
2023-12-22 09:05:45 +0300

    my todo upgrade

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddFrom from '../item-add-form';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  maxId = 100;
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    todoDate: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  }
+
+  deleteItem = (id) => {
+    this.setState(({todoDate})=>{
+      const idx = todoDate.findIndex((el)=>el.id === id)
+
+      const newArray = [
+        ...todoDate.slice(0, idx),
+        ...todoDate.slice(idx + 1)
+      ]
+
+      return {
+        todoDate: newArray
+      }
+    })
+  }
+
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++ 
+    }
+
+    this.setState(({todoDate})=>{
+      const newArr = [...todoDate, newItem]      
+      return {todoDate: newArr}
+    })
+    
+  }
+
+  render() {  
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList
+        todos={this.state.todoDate}
+        onDeleted={ this.deleteItem }/>
+
+        <ItemAddFrom onItemAdded={this.addItem}/>
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
-
-export default App
+    );
+  }
+}
